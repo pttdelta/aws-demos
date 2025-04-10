5b3be01fa9b09cb8fcb40162aa9527969d64a25e
Andy Frith <andyafrith@yahoo.com>
2025-04-10 16:20:52 +0100

    format

--- terraform.d/s3.d/s3.tf
+++ terraform.d/s3.d/s3.tf
@@ -90,8 +90,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "my_bucket_lifecycle" {
 resource "aws_s3_bucket_public_access_block" "my_block_public_access" {
   bucket = aws_s3_bucket.my_bucket.id
 
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
