d37ae2fcd1d6da84d7b5e026cca46a7cbfa84dca
Andy Frith <andyafrith@yahoo.com>
2025-04-10 16:27:25 +0100

    rotate kms key

--- terraform.d/s3.d/kms.tf
+++ terraform.d/s3.d/kms.tf
@@ -1,4 +1,5 @@
 # Create the KMS key (you can also use the default AWS-managed KMS key for S3)
 resource "aws_kms_key" "s3_key" {
   description = "KMS key for encrypting S3 bucket"
+  enable_key_rotation = true
 }
