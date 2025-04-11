65dc7b5a0c5655cf2e36a3d3aeabc0cec2475b27
Andy Frith <andyafrith@yahoo.com>
2025-04-11 10:03:11 +0100

    tflint ignore variable as not sure if this variable in downstream module or not at this stage.

--- terraform.d/eks.d/vars.tf
+++ terraform.d/eks.d/vars.tf
@@ -10,18 +10,14 @@ variable "eks_name" {
   default     = "deidre"
 }
 
-variable "env" {
-  description = "Target environment"
-  type        = string
-  default     = "dev"
-}
-
 variable "eks_cluster_version" {
   description = "Version of EKS cluster"
   type        = string
   default     = "1.32"
 }
 
+# TODO - address this apparently absent variable 
+# tflint-ignore: terraform_unused_declarations
 variable "enable_cluster_creator_admin_permissions" {
   description = "Indicates whether or not to add the cluster creator (the identity used by Terraform) as an administrator via access entry"
   type        = bool
